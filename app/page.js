7c9ad51bcbb44429f5578dbcccd15dcb7be306d7
ilyasbabu <ilyasbabuvp@gmail.com>
2023-08-01 00:12:52 +0530

    bugfix

--- app/page.js
+++ app/page.js
@@ -294,7 +294,7 @@ export default function Home() {
                   <svg width="1em" height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" ><path fill="#000000" d="M12 11.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5M12 2a7 7 0 0 0-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 0 0-7-7Z"></path></svg>
                 </div>
                 <div>
-                  <p>Skyroots Attestations No.CC 40/8942,(D-18), Jacob' DD Mall, MG Road Opp: Centre Square Mall, Kochi, Kerala 682035</p>
+                  <p>Skyroots Attestations No.CC 40/8942,(D-18), Jacob&apos; DD Mall, MG Road Opp: Centre Square Mall, Kochi, Kerala 682035</p>
                 </div>
               </div>
               <div className='flex'>
