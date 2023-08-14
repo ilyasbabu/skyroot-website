7729a052d72cef755d91abc53913c8f29aa8ac66
ilyasbabu <ilyasbabuvp@gmail.com>
2023-08-15 01:53:30 +0530

    toploader added

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +17,12 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header />
         <main className='mt-24 md:mt-24'>
+          <NextTopLoader
+            showSpinner={false}
+          />
           {children}
         </main>
-        <Footer/>
+        <Footer />
       </body>
     </html>
   )
